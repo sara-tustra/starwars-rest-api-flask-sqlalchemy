edc9bf970a6258d344039536da868c664f536e57
Sarai Santiago <saraisantiagom@gmail.com>
2021-04-30 02:47:00 -0400

    definiendo funcionalidad de favoritos y otorgando relaciones correctas a la base de datos

--- src/app.py
+++ src/app.py
@@ -1,150 +1,279 @@
-from flask import Flask, json, jsonify, request
+from flask import Flask, json, jsonify, request, render_template
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager 
-from models import db, User, Vehicles, Characters, Planets
+from flask_cors import CORS
+from models import db, User, Vehicle, Character, Planet, Favorite, VehicleFavorite, CharacterFavorite, PlanetFavorite
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.config['DEBUG'] = True
 app.config['ENV'] = 'development'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
 Migrate(app, db) #init, migrate, upgrade
+CORS(app)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-####### USERS ######
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    users = list(map(lambda user: user.to_dict(), users))
-    return jsonify(users), 200
 
-@app.route('/users', methods=['POST'])
-def add_new_user():
-    request_body = request.data
-    decoded_object = json.loads(request_body)
-    user = User()
-    user.username = decoded_object ['username']
-    user.email = decoded_object ['email']
-    user.password = decoded_object ['password']
-    user.save()
-    return jsonify(user.to_dict()), 201
+@app.route("/")
+def main():
+    return render_template('index.html')
 
 
-@app.route('/users/<int:id>', methods=['DELETE'])
-def delete_user(id):
-    user = User.query.get(id)
+@app.route('/users', methods=['GET', 'POST'])
+@app.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def users(id=None):
+    if request.method == 'GET':
+        if id is not None:
+            user = User.query.get(id)    
+            if not user:
+                return jsonify({"fail": "User not found"}), 404
+            return jsonify({
+                "success": "User found",
+                "user": user.serialize()
+            }), 200
+        else:
+            users = User.query.all()
+            users = list(map(lambda user: user.serialize(), users))
+            return jsonify({
+                "total": len(users),
+                "results": users
+            }), 200
 
-    if not user: 
-        return jsonify({"msg": "user not found"}), 404
-    user.delete()
-    return jsonify({"success": "user deleted"}), 200
+    if request.method == 'POST':
+        request_body = request.data
+        decoded_object = json.loads(request_body)
+        user = User()
+        user.username = decoded_object ['username']
+        user.email = decoded_object ['email']
+        user.password = decoded_object ['password']
+        user.save()
+        return jsonify({
+            "success": "user created!",
+            "user": user.serialize()
+        }), 201
+
+    if request.method == 'PUT':
+        pass
+    if request.method == 'DELETE':
+        user = User.query.get(id)
+        if not user: 
+            return jsonify({"fail": "user not found"}), 404
+        user.delete()
+        return jsonify({"success": "user deleted"}), 200
+
 
 ####### VEHICLES ######
-@app.route('/vehicles', methods=['GET'])
-def get_vehicles():
-    vehicles = Vehicles.query.all()
-    vehicles = list(map(lambda vehicle: vehicle.to_dict(), vehicles))
-    return jsonify(vehicles), 200
+@app.route('/vehicles', methods=['GET', 'POST'])
+@app.route('/vehicles/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def vehicles(id=None):
+    if request.method == 'GET':
+        if id is not None:
+            vehicle = Vehicle.query.get(id)    
+            if not vehicle:
+                return jsonify({"fail": "Vehicle not found"}), 404
+            return jsonify({
+                "success": "Vehicle found",
+                "vehicle": vehicle.serialize_vehicle_with_users()
+            }), 200
+        else:
+            vehicles = Vehicle.query.all()
+            vehicles = list(map(lambda vehicle: vehicle.serialize_vehicle_with_users(), vehicles))
+            return jsonify({
+                "total": len(vehicles),
+                "results": vehicles
+            }), 200
 
-@app.route('/vehicles', methods=['POST'])
-def add_new_vehicle():
-    request_body = request.data
-    decoded_object = json.loads(request_body)
-    vehicle = Vehicles()
-    vehicle.name = decoded_object['name']
-    vehicle.model = decoded_object['model']
-    vehicle.vehicle_class = decoded_object['vehicle_class']
-    vehicle.crew = decoded_object['vehicle_crew']
-    vehicle.manufacturer = decoded_object['manufacturer']
-    vehicle.cargo_capacity = decoded_object['cargo_capacity']
-    vehicle.cost_in_credits = decoded_object['cost_in_credits']
-    vehicle.consumables = decoded_object['consumables']
-    vehicle.save()
-    return jsonify(vehicle.to_dict()), 201
+    if request.method == 'POST':
+        request_body = request.data
+        decoded_object = json.loads(request_body)
+        vehicle = Vehicle()
+        vehicle.name = decoded_object ['name']
+        vehicle.model = decoded_object ['model']
+        vehicle.vehicle_class = decoded_object ['vehicle_class']
+        vehicle.crew = decoded_object['crew']
+        vehicle.manufacturer = decoded_object['manufacturer']
+        vehicle.cargo_capacity = decoded_object['cargo_capacity']
+        vehicle.cost_in_credits = decoded_object['cost_in_credits']
+        vehicle.consumables = decoded_object['consumables']
+        vehicle.save()
+        return jsonify({
+            "success": "vehicle created!",
+            "vehicle": vehicle.serialize_vehicle_with_users()
+        }), 201
 
+    if request.method == 'PUT':
+        pass
+    if request.method == 'DELETE':
+        vehicle = Vehicle.query.get(id)
+        if not vehicle: 
+            return jsonify({"fail": "vehicle not found"}), 404
+        vehicle.delete()
+        return jsonify({"success": "vehicle deleted"}), 200
 
-@app.route('/vehicles/<int:id>', methods=['DELETE'])
-def delete_vehicle(id):
-    vehicle = Vehicles.query.get(id)
-
-    if not vehicle: 
-        return jsonify({"msg": "vehicle not found"}), 404
-    vehicle.delete()
-    return jsonify({"success": "vehicle deleted"}), 200
 
 
 ####### CHARACTERS ######
 
-@app.route('/characters', methods=['GET'])
-def get_characters():
-    characters = Characters.query.all()
-    characters = list(map(lambda character: character.to_dict(), characters))
-    return jsonify(characters), 200
+@app.route('/characters', methods=['GET', 'POST'])
+@app.route('/characters/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def characters(id=None):
+    if request.method == 'GET':
+        if id is not None:
+            character = Character.query.get(id)    
+            if not character:
+                return jsonify({"fail": "Character not found"}), 404
+            return jsonify({
+                "success": "Character found",
+                "character": character.serialize_character_with_users()
+            }), 200
+        else:
+            characters = Character.query.all()
+            characters = list(map(lambda character: character.serialize_character_with_users(), characters))
+            return jsonify({
+                "total": len(characters),
+                "results": characters
+            }), 200
+    if request.method == 'POST':
+        request_body = request.data
+        decoded_object = json.loads(request_body)
+        character = Character()
+        character.name = decoded_object['name']
+        character.gender = decoded_object['gender']
+        character.hair_color = decoded_object['hair_color']
+        character.eye_color = decoded_object['eye_color']
+        character.height = decoded_object['height']
+        character.skin_color = decoded_object['skin_color']
+        character.birth_year = decoded_object['birth_year']
+        character.save()
 
-@app.route('/characters', methods=['POST'])
-def add_new_character():
-    request_body = request.data
-    decoded_object = json.loads(request_body)
-    character = Characters()
-    character.name = decoded_object['name']
-    character.gender = decoded_object['gender']
-    character.hair_color = decoded_object['hair_color']
-    character.eye_color = decoded_object['eye_color']
-    character.height = decoded_object['height']
-    character.skin_color = decoded_object['skin_color']
-    character.birth_year = decoded_object['birth_year']  
-    character.save()
-    return jsonify(character.to_dict()), 201
+        return jsonify({
+            "success": "character created!",
+            "character": character.serialize_character_with_users()
+        }), 201
 
-
-@app.route('/characters/<int:id>', methods=['DELETE'])
-def delete_character(id):
-    character = Characters.query.get(id)
-
-    if not character: 
-        return jsonify({"msg": "character not found"}), 404
-    character.delete()
-    return jsonify({"success": "character deleted"}), 200
+    if request.method == 'PUT':
+        pass
+    if request.method == 'DELETE':
+        character = Character.query.get(id)
+        if not character: 
+            return jsonify({"fail": "character not found"}), 404
+        character.delete()
+        return jsonify({"success": "character deleted"}), 200
 
 
 ####### PLANETS ######
 
-@app.route('/planets', methods=['GET'])
-def get_planets():
-    planets = Planets.query.all()
-    planets = list(map(lambda planet: planet.to_dict(), planets))
-    return jsonify(planets), 200
+@app.route('/planets', methods=['GET', 'POST'])
+@app.route('/planets/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def planets(id=None):
+    if request.method == 'GET':
+        if id is not None:
+            planet = Planet.query.get(id)    
+            if not planet:
+                return jsonify({"fail": "Planet not found"}), 404
+            return jsonify({
+                "success": "Planet found",
+                "planet": planet.serialize_planet_with_users()
+            }), 200
+        else:
+            planets = Planet.query.all()
+            planets = list(map(lambda planet: planet.serialize_planet_with_users(), planets))
+            return jsonify({
+                "total": len(planets),
+                "results": planets
+            }), 200
 
-@app.route('/planets', methods=['POST'])
-def add_new_planet():
-    request_body = request.data
-    decoded_object = json.loads(request_body)
-    planet = Planets()
-    planet.name = decoded_object['name']
-    planet.population = decoded_object['population']
-    planet.terrain = decoded_object['terrain']
-    planet.climate = decoded_object['climate']
-    planet.rotation_period = decoded_object['rotation_period']
-    planet.orbital_period = decoded_object['orbital_period']
-    planet.gravity = decoded_object['gravity']
-    planet.save()
-    return jsonify(planet.to_dict()), 201
+    if request.method == 'POST':
+        request_body = request.data
+        decoded_object = json.loads(request_body)
+        planet = Planet()
+        planet.name = decoded_object['name']
+        planet.population = decoded_object['population']
+        planet.terrain = decoded_object['terrain']
+        planet.climate = decoded_object['climate']
+        planet.rotation_period = decoded_object['rotation_period']
+        planet.orbital_period = decoded_object['orbital_period']
+        planet.gravity = decoded_object['gravity']
+        planet.save()
+        return jsonify({
+            "success": "planet created!",
+            "planet": planet.serialize_planet_with_users()
+        }), 201
+
+    if request.method == 'PUT':
+        pass
+    if request.method == 'DELETE':
+        planet = Planet.query.get(id)
+        if not planet: 
+            return jsonify({"fail": "planet not found"}), 404
+        planet.delete()
+        return jsonify({"success": "planet deleted"}), 200
 
 
-@app.route('/planets/<int:id>', methods=['DELETE'])
-def delete_planets(id):
-    planet = Planets.query.get(id)
 
-    if not planet: 
-        return jsonify({"msg": "planet not found"}), 404
-    planet.delete()
-    return jsonify({"success": "planet deleted"}), 200
+        
 
- 
+@app.route('/users/<int:user_id>/favorites', methods=['GET', 'POST', 'DELETE'])
+def favorites(user_id=None):
+    if request.method == 'GET':
+        if user_id is not None:
+            user = User.query.get(user_id)
+            if not user.favorites:
+                return jsonify({ "fail": "user does not have favorites" }), 404
+            
+            return jsonify({
+                "success": "Favorites found",
+                "favorites": user.favorites.serialize()
+                }),200
+        else:
+            users= User.query.all()
+            users = list(
+                map(lambda user_favs: user.serialize_with_favorites(), users)
+            )
 
+    if request.method == 'POST':
+        if user_id is None:
+            return jsonify({"fail": "please indicate a valid user"})
+        
+        vehicles = request.json.get('vehicles')
+        characters = request.json.get('characters')
+        planets = request.json.get('planets')
+
+        user = User() #Preguntar al profe
+        favorite = Favorite()
+        for vehicle_id in vehicles:
+            vehicle = Vehicle.query.get(vehicle_id)
+            favorite.vehicles.append(vehicle)
+
+        for character_id in characters:
+            character = Character.query.get(character_id)
+            favorite.characters.append(character)
+
+        for planet_id in planets:
+            planet = Planet.query.get(planet_id)
+            favorite.planets.append(planet)
+
+
+        user.favorites = favorites
+        favorite.save()
+        user.save()
+
+        
+        return jsonify({
+            "success": "favorites added",
+            "user": user.serialize_with_favorites()
+        })
+
+    
+
+
+
+            
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=3245, debug=True)
--- src/models.py
+++ src/models.py
@@ -2,12 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(250), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String (150), nullable=False)
-    
+    favorites = db.relationship(
+        'Favorite', cascade='all, delete', backref='user', uselist=False
+    )
+  
     def save(self):
         db.session.add(self) # INSERT
         db.session.commit() # SAVE THE INSERT
@@ -19,16 +22,29 @@ class User(db.Model):
         db.session.delete(self) # DELETE
         db.session.commit() # Guarda el DELETE
 
-    def to_dict(self):
+    def serialize(self):
         return {
             "id": self.id,
             "username": self.username,
             "email": self.email,
-            "password": self.password,           
+            "password": self.password           
         }
-
-
-class Vehicles(db.Model):
+    
+    def serialize_with_favorites(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "password": self.password,
+            "favorites": {
+                "vehicles": self.favorites.vehicles.serialize(),
+                "characters": self.favorites.characters.serialize(),
+                "planets": self.favorites.planets.serialize()
+            }
+        }
+   
+   
+class Vehicle(db.Model):
     __tablename__ = 'vehicles'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(250), nullable=False)
@@ -39,6 +55,7 @@ class Vehicles(db.Model):
     cargo_capacity = db.Column(db.Integer)
     cost_in_credits = db.Column(db.Integer)
     consumables = db.Column(db.Integer)
+    favorites = db.relationship('Favorite', secondary='vehicles_favorites', backref='vehicle')
 
     def save(self):
         db.session.add(self) # INSERT
@@ -51,20 +68,20 @@ class Vehicles(db.Model):
         db.session.delete(self) # DELETE
         db.session.commit() # Guarda el DELETE
 
-    def to_dict(self):
+    def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
             "model": self.model,
-            "vehicle class": self.vehicle_class,
+            "vehicle_class": self.vehicle_class,
             "crew": self.crew,
             "manufacturer": self.manufacturer,
             "cargo_capacity": self.cargo_capacity,
             "cost_in_credits": self.cost_in_credits,
             "consumables": self.consumables
         }
-
-class Characters(db.Model):
+    
+class Character(db.Model):
     __tablename__ = 'characters'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(250), nullable=False)
@@ -74,6 +91,7 @@ class Characters(db.Model):
     height = db.Column(db.String(250))
     skin_color = db.Column(db.String(250))
     birth_year = db.Column(db.String(250))
+    favorites = db.relationship('Favorite', secondary='characters_favorites', backref='character')
 
     def save(self):
         db.session.add(self) # INSERT
@@ -86,7 +104,7 @@ class Characters(db.Model):
         db.session.delete(self) # DELETE
         db.session.commit() # Guarda el DELETE
 
-    def to_dict(self):
+    def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
@@ -97,8 +115,8 @@ class Characters(db.Model):
             "skin_color": self.skin_color,
             "birth_year": self.birth_year
         }
-
-class Planets(db.Model):
+    
+class Planet(db.Model):
     __tablename__ = 'planets'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(300), nullable=False)
@@ -108,6 +126,7 @@ class Planets(db.Model):
     rotation_period = db.Column(db.String(300))    
     orbital_period = db.Column(db.String(300))
     gravity = db.Column(db.String(300))
+    favorites = db.relationship('Favorite', secondary='planets_favorites', backref='planet') ## preguntar al profe
 
     def save(self):
         db.session.add(self) # INSERT
@@ -120,7 +139,7 @@ class Planets(db.Model):
         db.session.delete(self) # DELETE
         db.session.commit() # Guarda el DELETE
 
-    def to_dict(self):
+    def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
@@ -131,3 +150,61 @@ class Planets(db.Model):
             "orbital_period": self.orbital_period,
             "gravity": self.gravity
         }
+
+
+## tabla de favoritos mas tablas pivote para cada categoria
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    vehicles = db.relationship('Vehicle', secondary='vehicles_favorites')
+    characters = db.relationship('Character', secondary='characters_favorites')
+    planets = db.relationship('Planet', secondary='planets_favorites')
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def serialize(self):
+        return {
+           "id": self.id,
+           "user": {
+               "id": self.user.id,
+               "username": self.user.username
+            },
+           "vehicles": self.get_vehicles(),
+           "characters": self.get_characters(),
+           "planets": self.get_planets()
+        }
+    def get_vehicles(self):
+        return list(map(lambda vehicle: vehicle.serialize(), self.vehicles))
+
+    def get_characters(self):
+        return list(map(lambda character: character.serialize(), self.characters))
+
+    def get_planets(self):
+        return list(map(lambda planet: planet.serialize(), self.planets))
+
+class VehicleFavorite(db.Model):
+    __tablename__='vehicles_favorites'
+    vehicles_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'), primary_key=True)
+    favorites_id = db.Column(db.Integer, db.ForeignKey('favorites.id'), primary_key=True)
+
+
+class CharacterFavorite(db.Model):
+    __tablename__='characters_favorites'
+    characters_id = db.Column(db.Integer, db.ForeignKey('characters.id'), primary_key=True)
+    favorites_id = db.Column(db.Integer, db.ForeignKey('favorites.id'), primary_key=True)
+
+class PlanetFavorite(db.Model):
+    __tablename__='planets_favorites'
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'), primary_key=True)
+    favorites_id = db.Column(db.Integer, db.ForeignKey('favorites.id'), primary_key=True)
